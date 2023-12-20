043a9d5b4b4c6600560b4d9948597beebbce144b
Nasimul Hasan <nasimulhasandeep@gmail.com>
2023-12-20 16:56:56 +0600

    Updates

---
+++ assets/admin.6fce7591.js
@@ -0,0 +1,1 @@
+import{_ as t,t as n,v as r,S as o}from"./entry.14e586fd.js";const s={};function a(e,c){return n(),r("div",null,[o(e.$slots,"default")])}const _=t(s,[["render",a]]);export{_ as default};
--- assets/auth.ccd67606.js
+++
@@ -1,1 +0,0 @@
-import{a6 as s,a7 as t,c as o,s as a}from"./entry.b72ab29f.js";const i=s((c,n)=>{const e=t();if(o(()=>e.access_token).value===null)return a("/login")});export{i as default};
