48dead659722734034c460c186e166658670c33f
Nasimul Hasan <nasimulhasandeep@gmail.com>
2023-12-20 18:29:52 +0600

    Updates

--- assets/admin.98f7ddf8.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,t as n,v as r,S as o}from"./entry.73658944.js";const s={};function a(e,c){return n(),r("div",null,[o(e.$slots,"default")])}const _=t(s,[["render",a]]);export{_ as default};
--- assets/guest.c5dd333e.js
+++
@@ -1,1 +0,0 @@
-import{a6 as s,a7 as t,c as o,s as a}from"./entry.73658944.js";const d=s((c,r)=>{const e=t();if(o(()=>e.access_token).value)return a("/")});export{d as default};
