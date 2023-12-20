0a8fa6401ae2f9da37dd450bd530271353d33b09
Nasimul Hasan <nasimulhasandeep@gmail.com>
2023-12-20 16:33:29 +0600

    Updates

---
+++ assets/auth.ccd67606.js
@@ -0,0 +1,1 @@
+import{a6 as s,a7 as t,c as o,s as a}from"./entry.b72ab29f.js";const i=s((c,n)=>{const e=t();if(o(()=>e.access_token).value===null)return a("/login")});export{i as default};
---
+++ assets/guest.3a37fd03.js
@@ -0,0 +1,1 @@
+import{a6 as s,a7 as t,c as o,s as a}from"./entry.b72ab29f.js";const d=s((c,r)=>{const e=t();if(o(()=>e.access_token).value)return a("/")});export{d as default};
